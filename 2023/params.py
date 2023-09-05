5dc1a72b540dd1f0b53f65ad061caf233bd5b73d
hanjiakang <33483638+hanjiakang@users.noreply.github.com>
2023-09-05 10:13:54 +0800

    Update params.py

--- 2023/params.py
+++ 2023/params.py
@@ -38,7 +38,7 @@ sys.path.insert(0, './')
 from ali2020_sims import simsLensing
 from utils import bl_eft
 from one import *
-from constant import *
+from library_parameter import *
 
 
 # Data Paths
