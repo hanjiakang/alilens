993916c092772af01db70da0514e4aa26c3222a8
hanjiakang <33483638+hanjiakang@users.noreply.github.com>
2023-10-01 10:37:37 +0800

    Update apodized.py

--- mask/masks/apodized.py
+++ mask/masks/apodized.py
@@ -6,7 +6,7 @@ import pymaster as nm
 print(1)
 mask=hp.read_map("/disk1/home/hanjk/2022/fg4lens/masks/AliCPT_UNPfg_filled_C_1024.fits")
 print(2)
-aposcale=5
+aposcale=2
 print(3)
 apomask = nm.mask_apodization(mask, aposcale, apotype="C1")
 print(4)
@@ -15,7 +15,7 @@ hp.write_map("apomask.fits",apomask, overwrite=True)
 print(1)
 mask=hp.read_map("/disk1/home/hanjk/2022/fg4lens/masks/AliCPT_UNPfg_filled_C_1024.fits")
 print(2)
-aposcale=5
+aposcale=2
 print(3)
 apomask = nm.mask_apodization(mask, aposcale, apotype="C1")
 print(4)
