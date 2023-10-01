b61b6d4f75ac68c4aca91b8bcb2817de11f37221
hanjiakang <1029881934@qq.com>
2023-10-01 10:32:58 +0800

    maskforground

---
+++ mask/masks/apodized.py
@@ -0,0 +1,22 @@
+import healpy as hp
+import numpy as np
+import matplotlib.pyplot as plt
+import pymaster as nm
+
+print(1)
+mask=hp.read_map("/disk1/home/hanjk/2022/fg4lens/masks/AliCPT_UNPfg_filled_C_1024.fits")
+print(2)
+aposcale=5
+print(3)
+apomask = nm.mask_apodization(mask, aposcale, apotype="C1")
+print(4)
+hp.write_map("apomask.fits",apomask, overwrite=True)
+
+print(1)
+mask=hp.read_map("/disk1/home/hanjk/2022/fg4lens/masks/AliCPT_UNPfg_filled_C_1024.fits")
+print(2)
+aposcale=5
+print(3)
+apomask = nm.mask_apodization(mask, aposcale, apotype="C1")
+print(4)
+hp.write_map("apomask_b.fits",apomask, overwrite=True)
